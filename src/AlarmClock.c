d296706d3d79ff234d69156b0905f77f5e8260a9
Anderson Felippe <adfelippe@gmail.com>
2020-06-26 22:46:05 -0300

    Code refactor

--- src/AlarmClock.c
+++ src/AlarmClock.c
@@ -8,7 +8,7 @@ typedef struct {
 static Alarm scheduledAlarms[MAX_ALARMS];
 
 static void scheduleAlarm(Alarm *scheduledAlarm, int time, AlarmCallback callback);
-
+static int8_t getAlarmTimePositionFromList(uint32_t time);
 
 void AlarmClock_Init(void)
 {
@@ -21,11 +21,11 @@ void AlarmClock_Init(void)
 
 int SetAlarmClockCallback(int time, AlarmCallback callback)
 {
-    if (time % ALARMS_MULTIPLE_VALID_VALUE != 0)
-        return ALARM_NOT_MULTIPLE_OF_100MS;
-
     if (time == 0)
         return ALARM_IS_ZERO;
+
+    if (time % ALARMS_MULTIPLE_VALID_VALUE != 0)
+        return ALARM_NOT_MULTIPLE_OF_100MS;
 
     if (IsAlarmClockTimeSet(time))
         return ALARM_ALREADY_SET;
@@ -54,30 +54,41 @@ uint32_t GetAmountOfSetAlarms(void)
 
 bool IsAlarmClockTimeSet(uint32_t time)
 {
-    for (uint8_t i = 0; i < MAX_ALARMS; i++) {
-        if (scheduledAlarms[i].time == time)
-            return true;
-    }
-
-    return false;
+    if (getAlarmTimePositionFromList(time) < 0)
+        return false;
+    else
+        return true;
 }
 
 AlarmCallback GetAlarmClockCallbackFromTime(uint32_t time)
 {
-    for (uint8_t i = 0; i < MAX_ALARMS; i++) {
-        if (scheduledAlarms[i].time == time)
-            return scheduledAlarms[i].callback;
-    }
 
-    return NULL;
+    int8_t alarmTimeIndexPosition = getAlarmTimePositionFromList(time);
+
+    if (alarmTimeIndexPosition < 0) {
+        return NULL;
+    } else {
+        return scheduledAlarms[alarmTimeIndexPosition].callback;
+    }
 }
 
 void RemoveAlarmClock(uint32_t time)
 {
+    int8_t alarmTimeIndexPosition = getAlarmTimePositionFromList(time);
+
+    if (alarmTimeIndexPosition >= 0) {
+        scheduledAlarms[alarmTimeIndexPosition].time = 0;
+    }
+}
+
+static int8_t getAlarmTimePositionFromList(uint32_t time)
+{
     for (uint8_t i = 0; i < MAX_ALARMS; i++) {
         if (scheduledAlarms[i].time == time)
-            scheduledAlarms[i].time = 0;
+            return i;
     }
+
+    return -1;
 }
 
 static void scheduleAlarm(Alarm *scheduledAlarm, int time, AlarmCallback callback)
